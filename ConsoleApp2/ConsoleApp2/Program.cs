b4b0e96260487c7e428c038de645655f0393aaa5
Emilka <s27827@pjwstk.edu.pl>
2024-03-15 17:51:31 +0100

    Implement max function - GetMax

---
+++ ConsoleApp2/ConsoleApp2/Program.cs
@@ -0,0 +1,18 @@
+﻿int[] arr = new[] { 11, 2, 3, 4, 5, 6, 6, 10};
+double max = GetMax(arr);
+Console.WriteLine(("Max value is " + max));
+
+
+static double GetMax(int[] arr)
+{
+    int temp=arr[0];
+    foreach (var num in arr)
+    {
+        if (num > temp)
+        {
+            temp = num;
+        }
+    }
+    
+    return temp;
+}
